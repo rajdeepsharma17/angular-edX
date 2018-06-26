4be19084087d30eb8a3bae84ea418b2cdb6f6a04
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-06-26 16:42:59 +0530

    added cached array in git-search Service

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
 import { GitSearch } from './git-search'
+import { resolve } from 'url';
+import { reject } from 'q';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitSearchService {
+  cachedValues: Array<{
+    [query: string]: GitSearch
+  }> = [];
 
   constructor() { }
+
+  gitSearch = (query: string)=> {
+    let promise = new Promise((resolve, reject)=> {
+      if (this.cachedValues[query]) {
+        resolve(this.cachedValues[query])
+      }else{
+        resolve("Placeholder")
+      }
+    })
+    return promise
+  }
 }
