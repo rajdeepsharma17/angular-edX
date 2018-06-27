49620e0817b37e5ca3df45c3498b463128f64852
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-06-27 15:57:27 +0530

    added routing parameters

--- src/app/git-search/git-search.component.ts
+++ src/app/git-search/git-search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GitSearchService } from '../git-search.service'
 import { GitSearch } from '../git-search'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, ParamMap, Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-git-search',
@@ -12,28 +13,39 @@ export class GitSearchComponent implements OnInit {
   searchResults: GitSearch;
   searchQuery: string;
   title: string;
+  displayQuery: string;
 
 
-  constructor(private GitSearchService: GitSearchService, private route: ActivatedRoute) { }
+  constructor(
+    private GitSearchService: GitSearchService,
+    private route: ActivatedRoute,
+    private router: Router ) { }
 
-  ngOnInit() {
-    this.GitSearchService.gitSearch('angular').then( (response) => {
-      this.searchResults = response;
-    }, (error) => {
-      alert("Error: " + error.statusText)
-    })
-    this.route.data.subscribe( (result) => {
-      this.title = result.title
-    });
+    ngOnInit() {
+      this.route.paramMap.subscribe( (params: ParamMap) => {
+        this.searchQuery = params.get('query');
+        this.displayQuery = params.get('query');
+        this.gitSearch();  
+      })
+      this.route.data.subscribe( (result) => {
+        this.title = result.title
+      });
+    }
+  
+    gitSearch = () => {
+      this.GitSearchService.gitSearch(this.searchQuery).then( (response) => {
+        this.searchResults = response;
+      }, (error) => {
+        alert("Error: " + error.statusText)
+      })
+    }
+  
+    sendQuery = () => {
+      this.searchResults = null;
+      this.router.navigate(['/search/' + this.searchQuery])
+    }
+  
   }
 
-  gitSearch = () => {
-    this.GitSearchService.gitSearch(this.searchQuery).then( (response) => {
-      this.searchResults = response;
-    }, (error) => {
-      alert("Error: " + error.statusText)
-    })
-  }
 
 
-}
