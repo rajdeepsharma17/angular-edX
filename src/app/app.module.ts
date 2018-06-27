3249b5dc7e9b46dff346c21d400cac66eb15367d
rajdeepsharma17 <rajdeep.sharma17@gmail.com>
2018-06-27 14:33:09 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,38 @@ import { GitSearchService } from './git-search.service';
 import { AppComponent } from './app.component';
 import { GitSearchComponent } from './git-search/git-search.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: '', 
+    component: HomePageComponent 
+  },
+  { path: 'search',      
+    component: GitSearchComponent,
+    data: { 
+      title: 'Git Search' 
+    } 
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    GitSearchComponent
+    GitSearchComponent,
+    HomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [GitSearchService],
   bootstrap: [AppComponent]
